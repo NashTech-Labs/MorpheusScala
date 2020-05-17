a973cfacce836ffbde5e422c7231ed14b8454cc9
Swantika Gupta <swantika.gupta@knoldus.com>
2020-05-17 13:41:46 +0530

    Add Log statements

--- src/main/scala/com/knoldus/CypherSpark/MorpheusScala.scala
+++ src/main/scala/com/knoldus/CypherSpark/MorpheusScala.scala
@@ -5,26 +5,29 @@ import org.opencypher.morpheus.api.MorpheusSession
 import org.opencypher.morpheus.api.io.MorpheusElementTable
 import org.opencypher.okapi.api.io.conversion.{NodeMappingBuilder, RelationshipMappingBuilder}
 import org.opencypher.okapi.api.value.CypherValue
+import org.slf4j.LoggerFactory
 
 
 object MorpheusScala extends App {
 
-    //Create a Spark Session
+    private val LOGGER = LoggerFactory.getLogger(this.getClass.getName)
+
+    LOGGER.info("Creating Spark Session")
     val spark = SparkSession
       .builder()
       .appName(s"${this.getClass.getSimpleName}")
       .config("spark.master","local[*]")
       .getOrCreate()
 
-    //Create a Morpheus session
+    LOGGER.info("Creating Morpheus session")
     implicit val morpheus: MorpheusSession = MorpheusSession.create(spark)
 
-    //Read csv files into data frame
+    LOGGER.debug("Reading csv files into data frames")
     val moviesDF = MovieActorDataFrames.createNode(spark, "movies.csv")
     val personsDF = MovieActorDataFrames.createNode(spark, "persons.csv")
     val actedInDF = MovieActorDataFrames.createNode(spark, "acted_in.csv")
 
-    //Create element mapping for movies node
+    LOGGER.info("Creating element mapping for movies node")
     val movieNodeMapping = NodeMappingBuilder
       .withSourceIdKey("id:Int")
       .withImpliedLabel("Movies")
@@ -36,7 +39,7 @@ object MorpheusScala extends App {
       .withPropertyKey(propertyKey = "rated", sourcePropertyKey = "rated")
       .build
 
-    //Create element mapping for person node
+    LOGGER.info("Creating element mapping for person node")
     val personNodeMapping = NodeMappingBuilder
       .withSourceIdKey("id:Int")
       .withImpliedLabel("Person")
@@ -45,7 +48,7 @@ object MorpheusScala extends App {
       .withPropertyKey("poster_image", "poster_image")
       .build
 
-    //Create element mapping for the edge between two nodes
+    LOGGER.info("Creating element mapping for the edge between two nodes")
     val actedInRelationMapping = RelationshipMappingBuilder
         .withSourceIdKey("rel_id:Int")
         .withSourceStartNodeKey("START_ID")
@@ -54,40 +57,40 @@ object MorpheusScala extends App {
         .withPropertyKey("role", "role")
         .build
 
-    //Create Nodes and Relationships
+    LOGGER.info("Creating nodes and edges using mapping")
     val moviesNode = MorpheusElementTable.create(movieNodeMapping, moviesDF)
     val personsNode = MorpheusElementTable.create(personNodeMapping, personsDF)
     val actedInRelation = MorpheusElementTable.create(actedInRelationMapping, actedInDF)
 
-    //Create Property Graph
+    LOGGER.info("Creating Property Graph")
     val actorMovieGraph = morpheus.readFrom(personsNode,moviesNode,actedInRelation)
 
 
-    //Query to get all Person nodes
+    LOGGER.info("Query Property Graph for the names of the actor nodes")
     val actors = actorMovieGraph.cypher(
         "MATCH (p:Person) return p.name AS Actor_Name"
     )
     actors.records.show
 
-    //Query to get all Movie nodes
+    LOGGER.info("Query to get titles of all Movie nodes")
     val movies = actorMovieGraph.cypher(
         "MATCH (m:Movies) return m.title AS Movie_Titles"
     )
     movies.records.show
 
-    //Query to read all actors and their respective movie
+    LOGGER.info("Query to read all actors and their respective movies")
     val actor_movies = actorMovieGraph.cypher(
         "MATCH (p:Person)-[a:ACTED_IN]->(m:Movies) RETURN p.name AS ACTOR_NAME, m.title AS MOVIE_TITLE"
     )
     actor_movies.records.show
 
-    //Query to read movie belonging to a particular actor
+    LOGGER.info("Query to read movie belonging to a particular actor")
     val movie = actorMovieGraph.cypher(
         "MATCH (p:Person{name:'Gloria Foster'}) -[a:ACTED_IN]->(m:Movies) RETURN m.title AS MOVIE_TITLE"
     )
     movie.records.show
 
-    //Query with Parameter Substitution
+    LOGGER.info("Query with Parameter Substitution")
     val param =  CypherValue.CypherMap(("movie_name", "The Matrix Revolutions"))
     val actorName = actorMovieGraph.cypher(
         s"MATCH (m:Movies{title:{movie_name}})<-[a:ACTED_IN]-(p:Person) RETURN p.name AS ACTOR_NAME",
--- src/main/scala/com/knoldus/CypherSpark/MovieActorDataFrames.scala
+++ src/main/scala/com/knoldus/CypherSpark/MovieActorDataFrames.scala
@@ -1,14 +1,18 @@
 package com.knoldus.CypherSpark
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.slf4j.LoggerFactory
 
 object MovieActorDataFrames {
+
+  private val LOGGER = LoggerFactory.getLogger(this.getClass.getName)
 
   val csvOptions = Map("header"->"true", "delimiter" -> ";", "inferSchema" -> "true")
 
   val resourcePath = "src/main/resources"
 
   def createNode(session: SparkSession, fileName: String): DataFrame = {
+    LOGGER.info(s"Reading data from $fileName")
     session.read.options(csvOptions).csv(s"$resourcePath/$fileName")
   }
 
